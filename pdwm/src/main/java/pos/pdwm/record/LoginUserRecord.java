6eab29d8a1c0a6ac0126dde76177bcb909bc769c
paulosavaris <pauloosavaris@gmail.com>
2024-03-23 20:50:55 -0300

    Cadastro funcionando

---
+++ pdwm/src/main/java/pos/pdwm/record/LoginUserRecord.java
@@ -0,0 +1,5 @@
+package pos.pdwm.record;
+
+public record LoginUserRecord(String LoginNome, String LoginEmail, String LoginSenha) {
+    
+}
--- pdwm/src/main/java/pos/pdwm/security/WebSecurityConfig.java
+++ pdwm/src/main/java/pos/pdwm/security/WebSecurityConfig.java
@@ -1,36 +1,38 @@
-package pos.pdwm.security;
+// package pos.pdwm.security;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.http.HttpMethod;
+// import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+// import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+// import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration
-@EnableWebSecurity
-public class WebSecurityConfig {
+// @Configuration
+// @EnableWebSecurity
+// public class WebSecurityConfig {
     
-    @SuppressWarnings("removal")
-    @Bean
-	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-			.csrf(csrf -> csrf
-					.disable())
-			.authorizeHttpRequests()
-            .requestMatchers("/**").permitAll()
-			.requestMatchers(HttpMethod.OPTIONS).permitAll()
-			.anyRequest()
-			.authenticated()
-			.and()
-			.formLogin((form) -> form
-							.loginPage("/login")
-							.defaultSuccessUrl("/users", true)
-							.permitAll()
-			)
-			.logout((logout) -> logout.permitAll());
+//     @SuppressWarnings("removal")
+//     @Bean
+// 	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//         http
+// 			.csrf(csrf -> csrf
+// 					.disable())
+// 			.authorizeHttpRequests()
+//             .requestMatchers("/css/**","/scripts/**").permitAll()
+// 			.requestMatchers("/login?formAction=cadastralogin").permitAll()
+// 			.requestMatchers("/login?error").permitAll() //ant seria mais apropriado, verificar o authorizeHttpRequests que esta "obsoleto"
+// 			.requestMatchers(HttpMethod.OPTIONS).permitAll()
+// 			.anyRequest()
+// 			.authenticated()
+// 			.and()
+// 			.formLogin((form) -> form
+// 							.loginPage("/login")
+// 							.defaultSuccessUrl("/users", true)
+// 							.permitAll()
+// 			)
+// 			.logout((logout) -> logout.permitAll());
 
-		return http.build();
-	}	
+// 		return http.build();
+// 	}	
     
-}
+// }
